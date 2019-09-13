495d2fbbbf289a2b82bf447ffb3d7a0bdb3d9892
vdvz <vizir.vadim@mail.ru>
2019-09-13 21:13:34 +0700

    ls

--- evm1/evm1/evm1.cpp
+++ evm1/evm1/evm1.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <sys/times.h>
 #include <unistd.h>
+#include<time.h>
 
 using namespace std;
 
@@ -22,26 +23,33 @@ int main() {
 	int expected_time = 15;
 	string data;
 	struct tms start, end;
+	struct timespec start_ts, end_ts;
 	const int max_buffer = 256;
 	long clocks_per_sec = sysconf(_SC_CLK_TCK);
 	long clocks;
 	char buffer[max_buffer];
-	FILE * stream = _popen("vmstat", "r");
+	FILE * stream = popen("vmstat", "r");
 	if (stream) {
 		while (!feof(stream))
 			if (fgets(buffer, max_buffer, stream) != nullptr) data.append(buffer);
-		_pclose(stream);
+		pclose(stream);
 	}
 	
-	if (stoi(data.substr(data.length() - 6, 2)) >= 40){
+	if (stoi(data.substr(data.length() - 6, 2)) <= 40){
 		times(&start);
 		function();
 		times(&end);
 		clocks = end.tms_utime - start.tms_utime;
 		printf("Time taken: %lf sec.\n", (double)clocks / clocks_per_sec);
 	}
-	else {
-	
+	else{
+		clock_gettime(CLOCK_MONOTONIC_RAW, &start_ts);
+		function();
+		clock_gettime(CLOCK_MONOTONIC_RAW, &end_ts);
+		long double time = end.tv_sec - start.tv_sec + 0.000000001 * (end.tv_nsec - start.tv_nsec);
+		if(true) {
+			printf("Time taken: %lf sec.\n", time);
+		}
 	}
 	return 0;
 }
