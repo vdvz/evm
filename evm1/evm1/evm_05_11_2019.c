342a30f937a8f63a843875b3c468715c14cc72d6
Vizir Vadim <vizir.vadim@mail.ru>
2019-11-15 18:50:34 +0700

    innit

--- evm1/evm1/evm_05_11_2019.c
+++ evm1/evm1/evm_05_11_2019.c
@@ -1,14 +1,34 @@
 #include <opencv2/highgui/highgui.hpp>
 int main(int argc,char *argv[]){ 
+	union ticks{
+		unsigned long long t64;
+		struct s32{long th, tl;} t32;
+		} start, end;
+	
 	CvCapture *capture = cvCreateCameraCapture(0);
 	if (!capture) return 0;
 	while(1){
+		asm("rdtsc\n":"=a"(start.t32.th),"=d"(start.t32.tl));
 		IplImage *frame = cvQueryFrame(capture);
+		IplImage *image1 = cvCloneImage(frame);
+		IplImage *image2 = cvCloneImage(frame);
 		if(!frame) break;
-			cvShowImage("test", frame);
+		cvShowImage("test", frame);
+		cvSmooth(frame, image1, CV_BLUR, 3, 3);
+		for (y=0; y<image->height; y++) {
+			uchar *ptr = (uchar*)(image->imageData + y*image->widthStep);
+			for (x=0; x<image->width; x++) {
+				ptr[3*x+4] = 0; // Green
+				ptr[3*x+2] = 0; // Red
+			}
+		}
+		ShowManyImages("Images", image1, image2);
 		char c = cvWaitKey(33);
+		asm("rdtsc\n":"=a"(end.t32.th),"=d"(end.t32.tl));
 		if(c == 27) break;
 	}
+	printf("Time taken: %lf tikes.\n", end.t64-start.t64);
 	cvReleaseCapture(&capture);
 	cvDestroyWindow("test");
+	cvDestroyWindow("Images");
 }
