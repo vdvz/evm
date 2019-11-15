9a61e61173637417b817fdd59092249908f3e2e6
vd_vz <vizir.vadim@mail.ru>
2019-11-15 20:09:49 +0700

    in

--- evm1/evm1/evm_05_11_2019.c
+++ evm1/evm1/evm_05_11_2019.c
@@ -1,4 +1,12 @@
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/highgui/highgui_c.h>
+#include "opencv2/imgproc/imgproc_c.h"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <iostream>
+#include <opencv2/core/core.hpp>
 int main(int argc,char *argv[]){ 
 	union ticks{
 		unsigned long long t64;
@@ -15,19 +23,19 @@ int main(int argc,char *argv[]){
 		if(!frame) break;
 		cvShowImage("test", frame);
 		cvSmooth(frame, image1, CV_BLUR, 3, 3);
-		for (y=0; y<image->height; y++) {
-			uchar *ptr = (uchar*)(image->imageData + y*image->widthStep);
-			for (x=0; x<image->width; x++) {
+		for (int y=0; y<image2->height; y++) {
+			uchar *ptr = (uchar*)(image2->imageData + y*image2->widthStep);
+			for (int x=0; x<image2->width; x++) {
 				ptr[3*x+4] = 0; // Green
 				ptr[3*x+2] = 0; // Red
 			}
 		}
-		ShowManyImages("Images", image1, image2);
+		cvShowImage("Images",  image2);
 		char c = cvWaitKey(33);
 		asm("rdtsc\n":"=a"(end.t32.th),"=d"(end.t32.tl));
 		if(c == 27) break;
 	}
-	printf("Time taken: %lf tikes.\n", end.t64-start.t64);
+	std::cout<<"Time taken: %lf tikes.\n"<< end.t64-start.t64;
 	cvReleaseCapture(&capture);
 	cvDestroyWindow("test");
 	cvDestroyWindow("Images");
