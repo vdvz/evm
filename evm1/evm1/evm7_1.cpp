23e51956bf64d3b515673a51a08d07ea089fc8d2
Vizir Vadim <vizir.vadim@mail.ru>
2019-12-04 20:19:48 +0700

    fix 7_1

--- evm1/evm1/evm7_1.cpp
+++ evm1/evm1/evm7_1.cpp
@@ -7,10 +7,13 @@ int main() {
 	clock_t t1 = clock();
 	const int N = 1024;
 	const int M = 10;
-	float matrix_I[N][N];
-	float matrix_A[N][N];
-	float matrix_B[N][N];
-	float matrix_R[N][N];
+	float *matrix_I = new float [N*N];
+	float *matrix_A  = new float [N*N];
+	float *matrix_B = new float [N*N];
+	float *matrix_R = new float [N*N];
+	float *matrix_E = new float [N*N];
+	float *matrix_prev = new float [N*N];
+	float *matrix_sub  = new float[N*N];
 /*	matrix_A[0][0] = 0;
 	matrix_A[0][1] = 1;
 	matrix_A[0][2] = 1;
@@ -28,8 +31,7 @@ int main() {
 	matrix_A[3][2] = 1;
 	matrix_A[3][3] = 0;
 */	
-	/*
-		for(int i = 0; i<N; i++){
+	/*for(int i = 0; i<N; i++){
 			for(int j = 0; j<N; j++){
 				matrix_A[i][j]=static_cast<float>(rand()) / (static_cast <float> (RAND_MAX/1000));;
 			}
@@ -37,9 +39,9 @@ int main() {
 	*/
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
-			matrix_I[i][j]=0;
+			matrix_I[i*N+j]=0;
 			if (i == j) {
-				matrix_I[i][j] += 1;
+				matrix_I[N*i+j] += 1;
 			}
 		}
 	}
@@ -48,7 +50,7 @@ int main() {
 	for (int j = 0; j < N; j++) {
 		float sum = 0;
 		for (int i = 0; i < N; i++) {
-			sum += matrix_A[i][j];
+			sum += matrix_A[i*N+j];
 
 		}
 		if (sum > norm_1) norm_1 = sum;
@@ -60,7 +62,7 @@ int main() {
 	for (int i = 0; i < N; i++) {
 		float sum = 0;
 		for (int j = 0; j < N; j++) {
-			sum += matrix_A[i][j];
+			sum += matrix_A[i*N+j];
 		}
 		if (sum > norm_inf) norm_inf = sum;
 	}
@@ -69,7 +71,7 @@ int main() {
 
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
-			matrix_B[i][j] = matrix_A[j][i] / (norm_1 * norm_inf);
+			matrix_B[N*i+j] = matrix_A[N*j+i] / (norm_1 * norm_inf);
 		}
 	}
 
@@ -82,9 +84,9 @@ int main() {
 
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
-			matrix_R[i][j] = matrix_I[i][j];
+			matrix_R[N*i+j] = matrix_I[N*i+j];
 			for (int k = 0; k < N; k++) {
-				matrix_R[i][j] -= matrix_B[i][k] * matrix_A[k][j];
+				matrix_R[N*i+j] -= matrix_B[N*i+k] * matrix_A[N*k+j];
 			}
 		}
 	}
@@ -99,14 +101,14 @@ int main() {
 
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
-			matrix_I[i][j] += matrix_R[i][j];
+			matrix_I[N*i+j] += matrix_R[N*i+j];
 		}
 	}
 	
 	float matrix_prev[N][N];
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
-			matrix_prev[i][j] = matrix_R[i][j];
+			matrix_prev[N*i+j] = matrix_R[N*i+j];
 		}
 	}
 
@@ -118,7 +120,6 @@ int main() {
 		std::cout << "\n";
 	}*/
 
-	float sub_matr[N][N];
 	  
 /*                for (int i = 0; i < N; i++) {
                         for (int j = 0; j < N; j++) {
@@ -130,13 +131,14 @@ int main() {
 	for (int itter = 0; itter < M-1; itter++) {
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				sub_matr[i][j] = 0;
+				matrix_sub[i*N+j] = 0;
 				for (int k = 0; k < N; k++) {
-					sub_matr[i][j] += matrix_R[i][k] * matrix_prev[k][j];
+					matrix_sub[i*N+j] += matrix_R[i*N+k] * matrix_prev[N*k+j];
 				}
 			}
 		}
-/*		std::cout << "MATRIX_I, ITTER: " << itter << "\n";
+		
+		/*std::cout << "MATRIX_I, ITTER: " << itter << "\n";
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
 				std::cout << matrix_I[i][j] << " ";
@@ -146,45 +148,50 @@ int main() {
 
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				matrix_I[i][j] += sub_matr[i][j];
+				matrix_I[i*N + j] += matrix_sub[N*i+j];
 			}
 		}
 
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N; j++) {
-				matrix_prev[i][j] = sub_matr[i][j];
+				matrix_prev[N*i+j] = matrix_sub[N*i+j];
 			}
 		}
 	
-	/*std::cout << "MATRIX_I, ITTER: " << itter << "\n";
-                for (int i = 0; i < N; i++) {
-                        for (int j = 0; j < N; j++) {
-                                std::cout << matrix_I[i][j] << " ";
-                        }
-                        std::cout << "\n";
-                }
-*/
+		/*std::cout << "MATRIX_I, ITTER: " << itter << "\n";
+		for (int i = 0; i < N; i++) {
+				for (int j = 0; j < N; j++) {
+						std::cout << matrix_I[i][j] << " ";
+				}
+				std::cout << "\n";
+		}*/
 	}
 
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
-			sub_matr[i][j] = 0;
+			matrix_sub[i*N+j] = 0;
 			for (int k = 0; k < N; k++) {
-				sub_matr[i][j] += matrix_I[i][k] * matrix_B[k][j];
+				matrix_sub[N*i+j] += matrix_I[N*i+k] * matrix_B[N*k+j];
 			}
 		}
 	}
-/*
-	for (int i = 0; i < N; i++) {
+/*for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
 			std::cout << sub_matr[i][j] << " ";
 		}
 		std::cout << "\n";
-	}
-*/
+	}*/
+	
 	clock_t t2 = clock();
 	std::cout << 1000.0 * (t2 - t1) / CLOCKS_PER_SEC << "\n";
 
-	system("pause");
+	delete matrix_I;
+	delete matrix_A;
+	delete matrix_B;
+	delete matrix_R;
+	delete matrix_E;
+	delete matrix_prev;
+	delete matrix_sub; 
+
 	return 0;
 }
