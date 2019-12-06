0c74c7f87710908db6eeea75a99a33428f7feed6
Vizir Vadim <vizir.vadim@mail.ru>
2019-12-06 22:18:18 +0700

    evm_05 oh goodnes

--- evm1/evm1/evm_05_11_2019.c
+++ evm1/evm1/evm_05_11_2019.c
@@ -11,17 +11,29 @@ int main(int argc,char *argv[]){
 	union ticks{
 		unsigned long long t64;
 		struct s32{long th, tl;} t32;
-		} start, end;
+	} start_frames, end_frames;
 	
+	struct timespec start, end, start_proc, end_proc;
+	float count_frames = 0;
+	double input_time=0;
+	double conversion_time=0;
+	double show_time=0;
+
 	CvCapture *capture = cvCreateCameraCapture(0);
 	if (!capture) return 0;
 	while(1){
-		asm("rdtsc\n":"=a"(start.t32.th),"=d"(start.t32.tl));
+		asm("rdtsc\n":"=a"(start_frames.t32.th),"=d"(start_frames.t32.tl));
+		count_frames++;
+		
+		clock_gettime(CLOCK_MONOTONIC_RAW, &start_proc);
 		IplImage *frame = cvQueryFrame(capture);
-		IplImage *image1 = cvCloneImage(frame);
-		IplImage *image2 = cvCloneImage(frame);
 		if(!frame) break;
-		cvShowImage("test", frame);
+		clock_gettime(CLOCK_MONOTONIC_RAW, &end_proc);
+		input_time += (end_proc.tv_sec - start_proc.tv_sec) + 0.000000001 * (end_proc.tv_nsec - start_proc.tv_nsec);
+		
+		clock_gettime(CLOCK_MONOTONIC_RAW, &start_proc);
+		IplImage *image1 = cvCloneImage(frame);
+		IplImage *image2 = cvCloneImage(frame);	
 		cvSmooth(frame, image1, CV_BLUR, 3, 3);
 		for (int y=0; y<image2->height; y++) {
 			uchar *ptr = (uchar*)(image2->imageData + y*image2->widthStep);
@@ -30,13 +42,28 @@ int main(int argc,char *argv[]){
 				ptr[3*x+2] = 0; // Red
 			}
 		}
-		cvShowImage("Images",  image2);
+		clock_gettime(CLOCK_MONOTONIC_RAW, &end_proc);
+		conversion_time += (end_proc.tv_sec - start_proc.tv_sec) + 0.000000001 * (end_proc.tv_nsec - start_proc.tv_nsec);
+		
+		clock_gettime(CLOCK_MONOTONIC_RAW, &start_proc);
+		cvShowImage("Image1",  image1);
+		cvShowImage("Image2",  image2);
+		clock_gettime(CLOCK_MONOTONIC_RAW, &end_proc);
+		show_time += (end_proc.tv_sec - start_proc.tv_sec) + 0.000000001 * (end_proc.tv_nsec - start_proc.tv_nsec);
+		
 		char c = cvWaitKey(33);
-		asm("rdtsc\n":"=a"(end.t32.th),"=d"(end.t32.tl));
+		asm("rdtsc\n":"=a"(end_frames.t32.th),"=d"(end_frames.t32.tl));
+		std::cout<<"Frames per second: "<< end_frames.t64-start_frames.t64 <<"\n";
 		if(c == 27) break;
 	}
-	std::cout<<"Time taken: %lf tikes.\n"<< end.t64-start.t64;
+	clock_gettime(CLOCK_MONOTONIC, &end);
+	printf("full time : %lf sec\n", time);
+	printf("fps : %lf \n", count / time);
+	printf("input time: %lf%%\n", input_time/time*100);
+	printf("conversion time: %lf%%\n", conversion_time/time*100);
+	printf("show time: %lf%%\n", show_time/time*100);
+	
 	cvReleaseCapture(&capture);
-	cvDestroyWindow("test");
-	cvDestroyWindow("Images");
+	cvDestroyWindow("Image1");
+	cvDestroyWindow("Image2");
 }
