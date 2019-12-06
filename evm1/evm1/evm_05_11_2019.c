7520c2ef52edd22c1ac5ad30d0d1ca865f4306ee
vd_vz <vizir.vadim@mail.ru>
2019-12-06 22:50:21 +0700

    evm5.c

--- evm1/evm1/evm_05_11_2019.c
+++ evm1/evm1/evm_05_11_2019.c
@@ -7,7 +7,7 @@
 #include "opencv2/imgproc/imgproc.hpp"
 #include <iostream>
 #include <opencv2/core/core.hpp>
-int main(int argc,char *argv[]){ 
+int main(){ 
 	union ticks{
 		unsigned long long t64;
 		struct s32{long th, tl;} t32;
@@ -34,7 +34,7 @@ int main(int argc,char *argv[]){
 		clock_gettime(CLOCK_MONOTONIC_RAW, &start_proc);
 		IplImage *image1 = cvCloneImage(frame);
 		IplImage *image2 = cvCloneImage(frame);	
-		cvSmooth(frame, image1, CV_BLUR, 3, 3);
+		//cvSmooth(frame, image1, CV_BLUR, 3, 3);
 		for (int y=0; y<image2->height; y++) {
 			uchar *ptr = (uchar*)(image2->imageData + y*image2->widthStep);
 			for (int x=0; x<image2->width; x++) {
@@ -52,16 +52,17 @@ int main(int argc,char *argv[]){
 		show_time += (end_proc.tv_sec - start_proc.tv_sec) + 0.000000001 * (end_proc.tv_nsec - start_proc.tv_nsec);
 		
 		char c = cvWaitKey(33);
-		asm("rdtsc\n":"=a"(end_frames.t32.th),"=d"(end_frames.t32.tl));
+	asm("rdtsc\n":"=a"(end_frames.t32.th),"=d"(end_frames.t32.tl));
 		std::cout<<"Frames per second: "<< end_frames.t64-start_frames.t64 <<"\n";
 		if(c == 27) break;
 	}
 	clock_gettime(CLOCK_MONOTONIC, &end);
-	printf("full time : %lf sec\n", time);
-	printf("fps : %lf \n", count / time);
-	printf("input time: %lf%%\n", input_time/time*100);
-	printf("conversion time: %lf%%\n", conversion_time/time*100);
-	printf("show time: %lf%%\n", show_time/time*100);
+	double time = (end.tv_sec - start.tv_sec) + 0.000000001 * (end.tv_nsec - start.tv_nsec);
+	std::cout<<"full time:"<< time<<" sec. \n";
+	std::cout<<"fps"<<count_frames / time<<"\n";
+	std::cout<<"input time: "<<input_time/time*100<<"\n";
+	std::cout<<"conversion time: "<<conversion_time/time*100<<"\n";
+	std::cout<<"show time: "<< show_time/time*100<<"\n";
 	
 	cvReleaseCapture(&capture);
 	cvDestroyWindow("Image1");
