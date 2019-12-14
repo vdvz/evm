643e9fab72bc2593724eac8c7e1e04ab23dd2198
Vizir Vadim <vizir.vadim@mail.ru>
2019-12-14 11:53:42 +0700

    mimim

--- evm1/evm1/evm8.cpp
+++ evm1/evm1/evm8.cpp
@@ -14,11 +14,12 @@ int main() {
 		unsigned long long t64;
 		struct s32{ long th, tl; } t32;
 	} start, end; 
-	//double cpu_Hz = 3000000000ULL;
 
 	int N = N_min;
 	int K = 10;
 	int i, k;
+	int value = 10;
+	double sub_time, time = 1111111111111.0;
 	while (N <= N_max) {
 		int* array = new int[N];
 		
@@ -31,24 +32,31 @@ int main() {
 		for (k = 0, i = 0; i < N; i++) k = array[k];
 
 		//Passage
+		for (int j = 0 ; value < j; j++){
 		asm("rdtsc\n":"=a"(start.t32.th), "=d"(start.t32.tl));
 		for (k = 0,i = 0; i < N * K; i++) k = array[k];
 		asm("rdtsc\n":"=a"(end.t32.th), "=d"(end.t32.tl));
-		std::cout <<"N: "<< N <<"\nTime per 1 elem straight passage: " << (end.t64 - start.t64) / K / N << " ticks.\n";
+		sub_time = (double)(end.t64 - start.t64);
+		if (sub_time < time) sub_time = time;
+		}
+		std::cout <<"N: "<< N <<"\nTime per 1 elem straight passage: " << time / K / N << " ticks.\n";
 
 		//Back passage
 		for (int i = N - 1; i >= 0; i--) {
 			array[i] = i - 1;
 		}
 		array[0] = N - 1;
-
 		//Warming
 		for (k = 0,i = 0; i < N; i++) k = array[k];
 		//Passage
+		for (int j = 0 ; value < j; j++){
 		asm("rdtsc\n":"=a"(start.t32.th), "=d"(start.t32.tl));
 		for (k = 0,i = 0; i < N * K; i++) k = array[k];
 		asm("rdtsc\n":"=a"(end.t32.th), "=d"(end.t32.tl));
-		std::cout << "Time per 1 elem back passage: " << (end.t64 - start.t64) / K / N << " ticks.\n";
+		sub_time = (double)(end.t64 - start.t64);
+		if (sub_time < time) sub_time = time;
+		}
+		std::cout << "Time per 1 elem back passage: " << time / K / N << " ticks.\n";
 
 		//Random passage
 		std::srand((unsigned)std::time(NULL));
@@ -65,10 +73,14 @@ int main() {
 		//Warming
 		for (k = 0,i = 0; i < N; i++) k = array[k];
 		//Passage
+		for (int j = 0 ; value < j; j++){
 		asm("rdtsc\n":"=a"(start.t32.th), "=d"(start.t32.tl));
 		for (k = 0,i = 0; i < N * K; i++) k = array[k];
 		asm("rdtsc\n":"=a"(end.t32.th), "=d"(end.t32.tl));
-		std::cout << "Time per 1 elem random passage: " << (end.t64 - start.t64) / K / N << " ticks.\n";
+		sub_time = (double)(end.t64 - start.t64);
+		if (sub_time < time) sub_time = time;
+		}
+		std::cout << "Time per 1 elem random passage: " << time / K / N << " ticks.\n";
 		delete []array;
 		N = N * 2;
 	}
